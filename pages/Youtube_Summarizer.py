966858e0d55e76da087bbdc281a8077c200b910e
Aryan <aryancr72000@gmail.com>
2024-02-07 23:53:19 +0530

    created yt summarizer

--- pages/Youtube_Summarizer.py
+++ pages/Youtube_Summarizer.py
@@ -1,7 +1,37 @@
 import streamlit as st 
+import os
+import google.generativeai as genai 
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi as ytapi 
+
+
+load_dotenv()
+api=os.getenv("api")
+genai.configure(api_key=api)
+
+def yt_transcript(link):
+    transcript=''
+    text=ytapi.get_transcript(link)
+    for i in range(len(text)):
+        transcript+=(text[i]["text"])+" "
+    return transcript
+
+def summarize(transcript):
+    
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(transcript+" summarize this in points")
+    return response
 
 st.write("# Paste link of video")
-title=st.text_input("link",placeholder="link")
 
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
+link=st.text_input("Link",placeholder="Paste link of video")
+
+if link:
+    link=link.split("=")[1]
+    st.image(f"https://img.youtube.com/vi/{link}/0.jpg")
+    with st.spinner("Processing"):
+        transcript=yt_transcript(link)
+    
+        response=summarize(transcript)
+
+        st.write(response.text)
